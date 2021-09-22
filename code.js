bfd1c52a194cae63238bc82ba1d1446bd57b3afc
Milon Hossain <81865918+fr-milon@users.noreply.github.com>
2021-09-22 12:01:01 +0600

    Add files via upload

---
+++ code.js
@@ -0,0 +1,15 @@
+function inword() {
+  
+    let result = writtenNumber(1234) ;
+    console.log(result);
+  
+  }
+  
+  function NUMBERTOTEXT(num) {
+    
+    let result = writtenNumber(num) ;
+    
+    return result;
+  
+  }
+  
